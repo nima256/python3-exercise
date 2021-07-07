529d1ce2fe52e777fb8fb498d5dc5264d97f0750
nimamahdi <nimamahdi1385@gmail.com>
2021-07-07 20:13:13 +0430

    if you do not have persian keyboard you can use this command

---
+++ farsi.py
@@ -0,0 +1,12 @@
+# in this command i know a persian language to code by dictionary 
+farsi = { 'a' : 'ا' , 'b' : 'ب' , 'd' : 'د' , 'e' : 'ع' , 'f' : 'ف' 
+, 'g' : 'گ' , 'h' : 'ح', 'h,' : 'ه' , 'i' : 'ی' , 'j' : 'ج' , 'k' : 'ک' 
+, 'l' : 'ل' , 'm' : 'م' , 'n' : 'ن' , 'p' : 'پ' , 'r' : 'ر' 
+, 's' : "س" , 's,' : 'ص' , 's,,' : 'ث' , 't' : 'ت' , 't,' : 'ط' 
+, 'v' : 'و', 'y' : 'ی' , 'z' : 'ز' , 'z,' : 'ض' , 'z,,' : 'ظ' 
+, 'z,,,' : 'ذ' , 'gh' : 'ق' , 'kh' : 'خ' , 'ch' : 'چ' , 'sh' : 'ش' 
+, 'zh' : 'ژ' }
+# here you can print alphabet that you want 
+print(farsi['s'],farsi['l'],farsi['a'],farsi['m'])
+print(farsi['kh'],farsi['v'],farsi['b'],farsi['i'])
+# and it will print for you
