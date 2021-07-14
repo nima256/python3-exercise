b74f0781ec4c68531fcab80413c9def37dc5c8e7
nimamahdi <nimamahdi1385@gmail.com>
2021-07-14 20:58:30 +0430

    if_whihtd.py

---
+++ if_whihtd.py
@@ -0,0 +1,8 @@
+number = int(input("please enter a number:"))
+if number == 100 :
+    print("its high number but no problem")
+elif number < 100 :
+    print("okay")
+
+else :
+    print("we can not save it")
