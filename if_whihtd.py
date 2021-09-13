826763d2a52b96c540d86ab48cb5544e88aa4690
nimamahdi <83651805+nimamahdi@users.noreply.github.com>
2021-09-13 13:19:31 +0430

    Delete if_whihtd.py

--- if_whihtd.py
+++
@@ -1,8 +0,0 @@
-number = int(input("please enter a number:"))
-if number == 100 :
-    print("its high number but no problem")
-elif number < 100 :
-    print("okay")
-
-else :
-    print("we can not save it")
