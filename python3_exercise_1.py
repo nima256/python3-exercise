7354a42baad6ee7a3fd960c492dc90a4cad11571
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-15 12:53:09 +0430

    variable exercise

---
+++ python3_exercise_1.py
@@ -0,0 +1,15 @@
+# hello  
+# here exercise for variable
+
+# here i creat a string 
+name = "nima"
+# and a integer
+age = 15
+# and i print them
+print(name)
+# output == nima
+print(age)
+# output == 15
+
+
+
