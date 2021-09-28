80cff14c331edfbb5f68a6114af2ba8eb0fbf69e
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-28 11:10:03 +0330

    exercise for list

---
+++ python3_exercise_03.py
@@ -0,0 +1,14 @@
+# hello
+# exercise for list
+#  here i want to give number if user and bring it in my list and sum it 
+
+
+a = []
+number = []
+for x in range(10):
+     list = int(input("please enter 10 number:"))
+     a = list
+     number.append(a)
+print(sum(number))
+
+# thanks for reading (:
