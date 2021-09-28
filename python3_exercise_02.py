2b1f38b0117f6485215f2051db3a42869f0445db
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-28 11:07:20 +0330

    if elif elseexersice

---
+++ python3_exercise_02.py
@@ -0,0 +1,12 @@
+# hello 
+# i hope you are good  
+# here "if , elif , else" exercise
+
+number = int(input("please enter a number:"))
+if number == 100 :
+    print("its high number but no problem")
+elif number < 100 :
+    print("okay")
+
+else :
+    print("we can not save it")
