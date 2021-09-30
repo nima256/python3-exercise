00b7c246e95796a8b818942995fe294a67802ceb
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-30 20:56:47 +0330

    exercise for list

---
+++ python3_exercise_06.py
@@ -0,0 +1,16 @@
+# exercise for list
+# that bring negative number and positive number for you
+
+
+# this code print negative number in list
+list = [-2 , 1 , 19 , -75 , -23 , -4 , 56]
+for x in list:
+    if x <= 0 :
+        print(x)
+# output ==> -2 , -75 , -23 , -4
+
+# this code print positive number in list
+list2 = [-2 , 1 , 19 , -75 , -23 , -4 , 56]
+for x in list2:
+    if x > 0 :
+        print(x)
